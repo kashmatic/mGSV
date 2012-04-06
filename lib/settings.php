2f4a01e27565ed776b2883d77cabc068a1b4e3ae
Kashi Revanna <Kashi.Revanna@unt.edu>
2012-04-06 09:55:00 -0500

    modified settings.php

--- lib/settings.php
+++ lib/settings.php
@@ -2,13 +2,8 @@
 
 $upload_dir = $_SERVER['DOCUMENT_ROOT'].'mgsv/tmp/';
 $database_name = 'mgsv';
-$database_user = 'gsv';
-$database_pass = '';
-$database_host = 'cas-qshare.cas.unt.edu';
-
-$database_name = 'MyTest';
-$database_user = 'root';
-$database_pass = 'password2011';
+$database_user = 'mgsvgsv';
+$database_pass = 'mgsvpass';
 $database_host = 'localhost';
 
 ?>
